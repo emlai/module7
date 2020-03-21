d03c59f11f2ab468bfcaa91f181f5fb807dfcc3c
Emil Laine <laine.emil@gmail.com>
2020-03-21 21:19:14 +0200

    Update to current Delta

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-delta -c src/*.delta -I%SDLDIR%\include -fms-extensions
-lld-link output.obj -LIBPATH:%SDLDIR%\lib\x64 SDL2.lib legacy_stdio_definitions.lib ucrt.lib msvcrt.lib -DEBUG
+delta -c src/*.delta -I%SDLDIR%\include -fms-extensions && lld-link output.obj -LIBPATH:%SDLDIR%\lib\x64 SDL2.lib legacy_stdio_definitions.lib ucrt.lib msvcrt.lib -DEBUG
